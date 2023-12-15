f6beb32dc76da6ec7fef9ebd8a8fb1c3a31182c1
Farioso Fernando <farioso.f@gmail.com>
2023-12-15 04:05:31 +0200

    ref

---
+++ test/file_import_test.dart
@@ -0,0 +1,6 @@
+import 'app.flutter' as ui;
+
+void main() {
+  print(ui.cocString);
+  print("Hello world");
+}
--- test/read_coc_file.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/services.dart';
-
-Future<String> readCocFile(String filePath) async {
-  try {
-    return await rootBundle.loadString(filePath);
-  } catch (e) {
-    throw ("Erro ao ler o arquivo .coc: $e");
-  }
-}
